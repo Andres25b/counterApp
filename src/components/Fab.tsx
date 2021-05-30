77bca0d9a3db3331e95fa3e6ce3a0338e6a2ba07
Jose Andres Herrera <andres25b.ha@hotmail.com>
2021-05-30 13:11:51 -0500

    Terminado

---
+++ src/components/Fab.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+export const Fab = ({title, onPress, position = 'br'}: Props) => {
+  const ios = () => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.75}
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}
+        onPress={onPress}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}
+          onPress={onPress}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'ios' ? ios() : android();
+};
+
+const styles = StyleSheet.create({
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+  right: {
+    right: 25,
+  },
+  left: {
+    left: 25,
+  },
+  fab: {
+    alignItems: 'center',
+    backgroundColor: '#5856D6',
+    borderRadius: 100,
+    height: 60,
+    justifyContent: 'center',
+    width: 60,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
